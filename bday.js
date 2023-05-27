0d2d883000b76c32ca4cbfd75c83293a847e0e38
arushibandi <arb.arushibandi@gmail.com>
2023-05-26 23:10:34 -0400

    ooook ty figma

--- bday.js
+++ bday.js
@@ -3,108 +3,19 @@ let pcSeq = 0;
 const pcs = new Map();
 // RTCDataChannel
 const dcs = new Map();
-let lastInvite = `invite.html`
-
-
-async function generateOffer() {
-  const apiKey = document.getElementById("api").value
-  pcSeq++
-  const pc = await newPeerConnection(apiKey);
-  pcs.set(pcSeq, pc)
-  console.log("pcs are now ", pc)
-  pc.onicecandidate = handleicecandidate(() => {
-    textelement = document.getElementById('lastinvite');
-    const offer = pc.localDescription;
-    console.log("unencoded", JSON.stringify(offer.toJSON()))
-    const details = document.getElementById("deets").value
-    const encoded = `invite.html?sdp=${encodeURIComponent(pc.localDescription.sdp)}&id=${pcSeq}&details=${encodeURIComponent(details)}&api=${apiKey}`
-    console.log("encoded", encoded)
-    textelement.href = encoded;
-    textelement.textContent = "link to last invite generated"
-    textelement.style = "color:#AC33FF;"
-    lastInvite = `${window.location.hostname}/${encoded}`
-  })
-
-  const dataChannel = pc.createDataChannel('rsvps');
-  dcs.set(pcSeq, dataChannel)
-  dataChannel.onopen = fakeServerOnClientOpen(pcSeq)
-  dataChannel.onmessage = fakeServerOnMessage
-
-  pc.onconnectionstatechange = (ev) => {
-    console.log("peer connection state change", ev)
-    console.log("PC", pc.currentRemoteDescription, pc.pendingRemoteDescription)
-    switch (pc.connectionState) {
-      case "connected":
-        console.log("the peers are connected")
-    }
-  }
-  pc.oniceconnectionstatechange = iceConnectionStateChange
-
-
-  let createOfferPromise = pc.createOffer();
-  createOfferPromise.then((offer) => {
-    console.log("done creating peer connection offer")
-    const setDesc = pc.setLocalDescription(offer);
-    setDesc.then(() => {
-      console.log("set local")
-    }).catch((reason) => {
-      console.error("set local failed: ", reason)
-    })
-  }).catch((reason) => {
-    console.error("peer connection offer failed: ", reason)
-  })
-}
-
-function iceConnectionStateChange(event) {
-  console.log('ice connection state: ' + event.target.iceConnectionState);
-}
-
+let lastInvite = `/invite.html`
 
 let rsvps = [
   "Arushi is attending the party and bringing a fun n flirty drinking game",
 ];
 
-function fakeServerOnMessage(message) {
-  console.log("got message", message)
-}
-
-function fakeServerOnClientOpen(channelId) {
-  return function(event) {
-    console.log("data channel got opened!!", event)
-    dc = dcs.get(channelId)
-    for (const rsvp of rsvps) {
-      dc.send(rsvp)
-    }
-  }
-}
-
-function gotRSVP() {
-  const blurb = document.getElementById("rsvpBlurb").value 
-  document.getElementById("rsvpBlurb").value = ""
-
-  const jsonBlurb = JSON.parse(blurb)
-
-  console.log("blurb", blurb)
-  console.log("json blurb", jsonBlurb)
-
-  const peerID = parseInt(jsonBlurb.id, 10)
-  const rsvp = `${jsonBlurb.name} is bringing ${jsonBlurb.bringing}`
-  rsvps.push(rsvp)
-
-  addToGuestList(rsvp)
-
-  pcs.get(peerID).setRemoteDescription({
-    "type": jsonBlurb.type,
-    "sdp": jsonBlurb.sdp
-  })
-
-  for (const [id, dc] of dcs) {
-    if (dc.readyState === "open") {
-      dc.send(rsvp)
-    }
-  }
-
-  return false
+function createInvite() {
+  const api = document.getElementById("api").value
+  const db = document.getElementById("db").value
+  const details = document.getElementById("details").value
+  lastInvite = `${window.location.hostname}/invite.html?api=${api}&db=${db}&details=${details}`
+  document.getElementById("inviteLink").href= lastInvite
+  document.getElementById("inviteLink").style = "color:#e230f2"
 }
 
 function copyLastInvite() {
---
+++ util.js
@@ -0,0 +1,54 @@
+var emojis = [
+	'👀','🥺','😡','🏄🏾‍♀️','🤪'
+];
+
+function randomEmoji() {
+  const emoji =  emojis[Math.floor(Math.random() * emojis.length)];
+  const hex = emoji.codePointAt(0).toString(16)
+  const emo = String.fromCodePoint("0x"+hex);
+  return emo
+};
+
+function getApiKey() {
+    return getQueryVariable("api")
+}
+
+function getFileKey() {
+    return getQueryVariable("db")
+}
+
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+      var pair = vars[i].split("=");
+      if (pair[0] == variable) {
+        return pair[1];
+      }
+    } 
+    return null
+}
+
+async function createComment(attendee,  bringing) {
+    await fetch(`https://api.figma.com/v1/files/${getFileKey()}/comments`, {
+        method: 'POST',
+        body: JSON.stringify({ 
+            "message":  `${Math.floor(Math.random() * emojis.length)} * ${attendee} is bringing ${bringing}`,
+        }),
+        headers: {
+            "X-Figma-Token": getApiKey(),
+            "Content-Type": "application/json; charset=UTF-8"
+        }
+    });
+}
+
+async function fetchComments() {
+    const resp = await fetch(`https://api.figma.com/v1/files/${getFileKey()}/comments`, {
+        headers: {
+            'X-Figma-Token': getApiKey(),
+            "Content-Type": "application/json; charset=UTF-8"
+        }
+    });
+    const comments = await resp.json()
+    return comments.comments
+}
